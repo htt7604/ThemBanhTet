b9755f7aefe2fab6051f9da5e0a0dae371cfddd5
Hồ Tấn Tài <hotantai0902@gmail.com>
2025-01-12 23:20:20 +0700

    1

--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -5,16 +5,18 @@ namespace WinFormsApp1
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+        //[STAThread]
+        //static void Main()
+        //{
+        //    // To customize application configuration such as set high DPI settings or default font,
+        //    // see https://aka.ms/applicationconfiguration.
+        //    ApplicationConfiguration.Initialize();
+        //    Application.Run(new Form1());
         }
 
         //dang thu  nghiem
+        //day la phan cua loi lam 
+        //
     }
 
 }
